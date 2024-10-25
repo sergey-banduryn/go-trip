325be67c7408ad805f266507d0aa3b7ca3d75a5b
Sergey Banduryn <sergey_b89@i.ua>
2024-10-25 17:14:40 +0300

    destinations slider

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// $(document).ready(function () {
+//   $(".owl-carousel").owlCarousel({
+//     center: true,
+//     loop: true,
+//     margin: 30,
+//     // startPosition: 1,
+//     items: 3,
+//   });
+// });
+
+var owl = $(".owl-carousel");
+owl.owlCarousel({
+  center: true,
+  loop: true,
+  margin: 30,
+  // startPosition: 1,
+  items: 3,
+});
+// Go to the next item
+$(".slider__btn--next").click(function () {
+  owl.trigger("next.owl.carousel");
+});
+// Go to the previous item
+$(".slider__btn--prev").click(function () {
+  // With optional speed parameter
+  // Parameters has to be in square bracket '[]'
+  owl.trigger("prev.owl.carousel", [300]);
+});
