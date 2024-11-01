f003a498c55bae852138f2f1fd9313266d606e99
Sergey Banduryn <sergey_b89@i.ua>
2024-11-01 16:16:27 +0200

    @media (max-width: 1000px) destinations

--- js/main.js
+++ js/main.js
@@ -12,9 +12,20 @@ var owl = $(".owl-carousel");
 owl.owlCarousel({
   center: true,
   loop: true,
-  margin: 30,
-  // startPosition: 1,
+  margin: 20,
   items: 3,
+  startPosition: 1,
+  responsive: {
+    0: {},
+    480: {},
+    850: {},
+    1000: {
+      margin: 30,
+    },
+    1200: {
+      margin: 30,
+    },
+  },
 });
 // Go to the next item
 $(".slider__btn--next").click(function () {
